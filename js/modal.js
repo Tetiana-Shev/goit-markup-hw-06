ab35a568726531512ec332a48d0d17065fddf11b
Tetiana-Shev <azaliya939@gmail.com>
2022-06-17 23:03:50 +0300

    Исправляет ошибки

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,11 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+  });
+})();
